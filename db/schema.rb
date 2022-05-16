62a7e29b8d2fda709538b9c0b4c002d1799b43d4
Philippine Lorrain <philippinelorrain@gmail.com>
2022-05-16 17:31:30 -0400

    migration hot fix

--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,17 @@ ActiveRecord::Schema.define(version: 2022_05_16_205743) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "cameras", force: :cascade do |t|
+    t.string "model"
+    t.integer "price_per_day"
+    t.string "zip_code"
+    t.text "details"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_cameras_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -29,4 +40,5 @@ ActiveRecord::Schema.define(version: 2022_05_16_205743) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "cameras", "users"
 end

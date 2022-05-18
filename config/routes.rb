bfcb376b90b85bdea47b9a85c7ff726e68aec6e6
Reem Gawad <reemabdelg@gmail.com>
2022-05-18 11:43:30 -0400

    Created rentals controller + index action and view

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :cameras
+  resources :cameras do
+    resources :rentals, only: [ :new, :create ]
+  end
+
+  resources :rentals, only: [ :index, :show, :destroy ]
 end

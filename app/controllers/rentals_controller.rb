d50c7c79bc88ea9093199a6db2c8176785491fbd
Reem Gawad <reemabdelg@gmail.com>
2022-05-18 13:39:07 -0400

    Created new+create+show actions, new+show pages

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -3,15 +3,32 @@ class RentalsController < ApplicationController
     @rentals = Rental.all
   end
 
-  # def show
-  # end
+  def show
+    @rental = Rental.find(params[:id])
+  end
 
   def new
+    @camera = Camera.find(params[:camera_id])
+    @rental = Rental.new
   end
 
   def create
+    @rental = Rental.new(rental_params)
+    @camera = Camera.find(params[:camera_id])
+    @user = User.find(current_user.id)
+    @rental.camera = @camera
+    @rental.user = @user
+    @rental.save
+
+    redirect_to rental_path(@rental)
   end
 
   def destroy
+  end
+
+  private
+
+  def rental_params
+    params.require(:rental).permit(:start_date, :end_date)
   end
 end

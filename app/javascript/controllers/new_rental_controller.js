c8be60b3f9fc1db2ffc8b3f2e14d9872ae853d12
Reem Gawad <reemabdelg@gmail.com>
2022-05-19 11:59:22 -0400

    Attempted to implement stimulus

--- app/javascript/controllers/new_rental_controller.js
+++ app/javascript/controllers/new_rental_controller.js
@@ -3,6 +3,6 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   static targets = ["start_date", "end_date"]
   calc() {
-    console.log("hello from new_rental_controller!")
+    console.log(this.start_dateTarget.innerText)
   }
 }

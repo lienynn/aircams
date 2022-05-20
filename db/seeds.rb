20294259b905f08b04b77fbc969f64c3956dba61
Lien Nguyen <lienhnguyen@outlook.com>
2022-05-20 12:17:17 -0400

    seed-update-added 1 more camera/put real address/created more rentals/created user_three

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,6 +16,7 @@ User.destroy_all
 user_one = User.create!(
   {
     email: "g@g.g",
+    first_name: "Philippine",
     password: "secret"
   }
 )
@@ -23,6 +24,15 @@ user_one = User.create!(
 user_two = User.create!(
   {
     email: "p@p.p",
+    first_name: "Reem",
+    password: "secret"
+  }
+)
+
+user_three = User.create!(
+  {
+    email: "r@r.r",
+    first_name: "Wagon",
     password: "secret"
   }
 )
@@ -32,7 +42,7 @@ camera_one = Camera.new(
   {
     model: "Canon EOS REBEL T7",
     price_per_day: 15,
-    address: Faker::Address.full_address,
+    address: "210 Ave Lepage, Dorval",
     details: "Comes with 18-55mm f lens. DSLR form factor with fine JPEG quality."
   }
 )
@@ -44,19 +54,19 @@ camera_two = Camera.new(
   {
     model: "Nikon F",
     price_per_day: 20,
-    address: Faker::Address.full_address,
+    address: "2363 rue Montgomery, Montreal",
     details: "Vintage camera. Comes with original finder and Nikkor-S 50mm lens."
   }
 )
 camera_two.photo.attach(io: File.open('app/assets/images/nikonf.jpeg'), filename: 'nikonf.jpeg')
-camera_two.user = user_two
+camera_two.user = user_one
 camera_two.save!
 
 camera_three = Camera.new(
   {
     model: "Sony Alpha A7C",
     price_per_day: 20,
-    address: Faker::Address.full_address,
+    address: "5333 Ave Casgrain, Montréal, QC H2T 1X3",
     details: "Compact full-frame camera and lens combo. Very good image quality and noise performance! Great battery life."
   }
 )
@@ -68,19 +78,19 @@ camera_four = Camera.new(
   {
     model: "Fujifilm x100v",
     price_per_day: 30,
-    address: Faker::Address.full_address,
+    address: "2313 Saint-Catherine St W, Montreal, Quebec H3H 1N2",
     details: "Beautiful camera with a vintage look. Great digital travel camera with fixed lens. Easy to use."
   }
 )
 camera_four.photo.attach(io: File.open('app/assets/images/fujifilmx100v.jpeg'), filename: 'fujifilmx100v.jpeg')
-camera_four.user = user_one
+camera_four.user = user_two
 camera_four.save!
 
 camera_five = Camera.new(
   {
     model: "Pentax K1000",
     price_per_day: 25,
-    address: Faker::Address.full_address,
+    address: "95 Chemin de la Côte-Saint-Antoine, Westmount",
     details: "Fully manual 35mm SLR, uses Kodak Plus-X 125 film. Takes beautiful photos. Excellent camera for all users."
 
   }
@@ -88,6 +98,18 @@ camera_five = Camera.new(
 camera_five.photo.attach(io: File.open('app/assets/images/pentaxk1000.jpeg'), filename: 'pentaxk1000.jpeg')
 camera_five.user = user_one
 camera_five.save!
+
+camera_six = Camera.new(
+  {
+    model: "Canon EOS R",
+    price_per_day: 27,
+    address: "2613 Notre-Dame St W, Montreal, Quebec H3J 1N9",
+    details: "Comes with 24-105mm f/4 Lens. Fast continuous shooting, great for movement photos in nature."
+  }
+)
+camera_six.photo.attach(io: File.open('app/assets/images/canoneosr.png'), filename: 'canoneosr.png')
+camera_six.user = user_two
+camera_six.save!
 
 # # rentals
 rental_one = Rental.new(
@@ -106,8 +128,8 @@ rental_two = Rental.new(
     end_date: Faker::Date.forward(days: 5)
   }
 )
-rental_two.user = user_one
-rental_two.camera = camera_two
+rental_two.user = user_three
+rental_two.camera = camera_four
 rental_two.save!
 
 rental_three = Rental.new(
@@ -120,3 +142,25 @@ rental_three = Rental.new(
 rental_three.user = user_two
 rental_three.camera = camera_five
 rental_three.save!
+
+rental_four = Rental.new(
+  {
+    start_date: Date.today,
+    end_date: Faker::Date.forward(days: 14)
+  }
+)
+
+rental_four.user = user_one
+rental_four.camera = camera_four
+rental_four.save!
+
+rental_five = Rental.new(
+  {
+    start_date: Date.today,
+    end_date: Faker::Date.forward(days: 20)
+  }
+)
+
+rental_five.user = user_three
+rental_five.camera = camera_one
+rental_five.save!
